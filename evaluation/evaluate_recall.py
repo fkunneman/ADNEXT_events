bbd990ead7c0d4a17cee96305135add94699cc0f
Florian Kunneman <f.kunneman@gmail.com>
2015-09-28 11:21:54 +0200

    fix

--- evaluation/evaluate_recall.py
+++ evaluation/evaluate_recall.py
@@ -12,8 +12,8 @@ tfs = sys.argv[4:] # stream of tweets
 # 1: read in gold standard --> sorted event term - time
 print('Reading gold standard file')
 gold_standard = []
-with open(fgs) as gs:
-    for line in fgs.readlines():
+with open(fgs, encoding = 'utf-8') as gs:
+    for line in gs.readlines():
         event_date = line.strip().split('\t')
         event_date[1] = time_functions.return_datetime(event_date[1])
         gold_standard.append(event_date)
@@ -22,7 +22,7 @@ gold_standard = sorted(gold_standard, key = lambda k : k[1])
 # 2: read in extracted events --> sorted event terms, time
 print('Reading extracted events file')
 extracted_events = []
-with open(fee) as ee:
+with open(fee, encoding = 'utf-8') as ee:
     for line in ee.readlines():
         date_event = line.strip().split('\t')
         event_date = [date_event[1].split(', '), time_functions.return_datetime(date_event[0], setting = 'vs')]
