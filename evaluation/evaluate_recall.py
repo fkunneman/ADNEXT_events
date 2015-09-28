2f183844bdb6df939f43a5fafb4ef3c29dcc103c
Florian Kunneman <f.kunneman@gmail.com>
2015-09-28 15:15:34 +0200

    fi

--- evaluation/evaluate_recall.py
+++ evaluation/evaluate_recall.py
@@ -1,13 +1,14 @@
 
 import sys
 import datetime
+import re
 
 import time_functions
 
 fgs = sys.argv[1] # gold standard
 fee = sys.argv[2] # extracted events
 of = sys.argv[3] # outfile
-tfs = sys.argv[4:] # stream of tweets
+#tfs = sys.argv[4:] # stream of tweets
 
 # 1: read in gold standard --> sorted event term - time
 print('Reading gold standard file')
@@ -17,6 +18,7 @@ gold_standard = []
 with open(fgs, encoding = 'utf-8') as gs:
     for line in gs.readlines():
         event_date = line.strip().split('\t')
+        #print(event_date)
         event_date[1] = time_functions.return_datetime(event_date[1])
         if event_date[0] not in gold_standard_events:
             gold_standard.append(event_date)
@@ -24,9 +26,9 @@ with open(fgs, encoding = 'utf-8') as gs:
             event_hashtag = '#' + ''.join(event_date[0].split())
             gold_standard_events_hashtag.append(event_hashtag)
 gold_standard = sorted(gold_standard, key = lambda k : k[1])
-# for event in gold_standard:
-#     sys.stdout.buffer.write(event[0].encode('utf8'))
-#     print(' ', event[1])
+#for event in gold_standard_events:
+#    sys.stdout.buffer.write(event.encode('utf8'))
+#    print(' ', event[1])
     
 # 2: read in extracted events --> sorted event terms, time
 print('Reading extracted events file')
@@ -39,39 +41,67 @@ with open(fee, encoding = 'utf-8') as ee:
 extracted_events = sorted(extracted_events, key = lambda k : k[1])
 
 # 3: read in tweets
-print('Reading tweet files')
-tweets = []
-for tweetfile in tfs:
-    with open(tweetfile, encoding = 'utf-8') as tf:
-        for tweet in tf.readlines()[1:]:
-            columns = tweet.strip().split('\t')
-            tweets.append(columns[-1])
+#print('Reading tweet files')
+#tweets = []
+#for tweetfile in tfs:
+#    with open(tweetfile, encoding = 'utf-8') as tf:
+#        for tweet in tf.readlines()[1:]:
+#            columns = tweet.strip().split('\t')
+#            tweets.append(columns[-1])
 
 # match extracted events with gold standard
 matches = []
 non_matches = []
 for event_date in extracted_events:
-    event_terms = event_date[0].split(', ')
+    event_terms = event_date[0]
     match = False
+    event_terms_str = ', '.join(event_date[0])
+#    print(et)
     for event_term in event_terms:
+  #          print(event_terms)
+  #      except:
+  #          continue
         if event_term[0] == '#':
+ #           try:
+ #               print('match', event_term)
+ #           except:
+ #               continue
             if event_term in gold_standard_events_hashtag:
-                matches.append(event_date[0])
+                matches.append(event_terms_str)
                 match = True
                 break
         else:
-            for part in event_term.split(' '):
-                if part in gold_standard_events:
-                    matches.append(event_date[0])
-                    match = True
+            parts = event_term.split(' ')
+            et = [event_term]
+            for l in range(2, len(parts)):
+                if l == 2:
+                    et.extend([' '.join(x) for x in zip(parts, parts[1:])])
+                elif l == 3:
+                    et.extend([' '.join(x) for x in zip(parts, parts[1:], parts[2:])])
+                elif l == 4:
+                    et.extend([' '.join(x) for x in zip(parts, parts[1:], parts[2:], parts[3:])])
+                elif l == 5:
+                    et.extend([' '.join(x) for x in zip(parts, parts[1:], parts[2:], parts[3:], parts[4:])])
+  #      try:
+            for part in et:
+#                try:
+#                    print('match', part)
+#                except:
+#                    continue
+                for ev in gold_standard:
+                    if re.match(part, ev[0]) or part in ev[0].split():
+                        matches.append((event_terms_str, ev[0], str(ev[1].date())))
+                        match = True
+                        break
+                if match:
                     break
             if match:
                 break
     if not match:
-        non_matches.append(event_date[0])
+        non_matches.append(event_terms_str)
 
-with open(of[:-4] + '_matches.txt') as outfile:
-    outfile.write('\n'.join(matches))
+with open(of[:-4] + '_matches.txt', 'w', encoding = 'utf-8') as outfile:
+    outfile.write('\n'.join(['\t-\t'.join(x) for x in matches]))
 
-with open(of[:-4] + '_non-matches.txt') as outfile:
-    outfile.write('\n'.join(non_matches))
+with open(of[:-4] + '_non-matches.txt', 'w', encoding = 'utf-8') as outfile:
+    outfile.write('\n'.join(['\t'.join(x) for x in non_matches]))
