003c3ca9644972b2247bc0362f5b0ff806d0906d
fkunneman <f.kunneman@let.ru.nl>
2015-11-13 11:42:19 +0100

    new query components

---
+++ framework/collect_tweets_keyterms.py
@@ -0,0 +1,72 @@
+
+import sys
+import query_defs
+
+keyterms = sys.argv[1]
+outdir = sys.argv[2]
+stats_tweets = sys.argv[3:]
+
+events = []
+event_terms = []
+with open(keyterms, 'r', encoding = 'utf-8') as kts:
+    for line in kts.read().split('\n'):
+        tokens = line.split('\t')
+        keyterms = tokens[1].split(',')
+        date_begin = tokens[2]
+        date_end = tokens[3]
+        events.append([keyterms, date_begin, date_end])
+        terms.extend(keyterms)
+
+half = len(stats_tweets) / 2
+stats = stats_tweets[:half]
+tweets = stats_tweets[half:]
+date_statfile = {}
+date_tweetfile = {}
+for stat in stats:
+    date = int(stat.split('/')[-1][:8])
+    date_statfile[date] = stat
+for tweet in tweets:
+    date = int(tweet.split('/')[-1][:8])
+    date_tweetfile[date] = tweet
+
+term_date_tweets = defaultdict(lambda : {})
+dates = sorted([int(x.split('/')[-1][:8]) for x in stats])
+for date in dates:
+    stat = date_statfile[date]
+    with open(stat, 'r', encoding = 'utf-8') as stat_open:
+        statlines = stat_open.readlines()
+    tweet = date_tweetfile[date]
+    with open(tweet, 'r', encoding = 'utf-8') as tweet_open:
+        tweetlines = tweet_open.readlines()
+    for line in statlines:
+        tokens = line.strip().split('\t')
+        term = tokens[0]
+        if len(list(set(term) & event_terms_set)) > 0:
+            begin = int(tokens[2])
+            end = int(tokens[3])
+            tweets = tweetlines[begin:end]
+            term_date_tweets[term][date] = tweets
+
+for event in events:
+    print(event)
+    event_terms = event[0]
+    date_begin = event[1]
+    date_end = event[2]
+    date_begin_index = dates.index(date_begin)
+    date_end_index = dates.index(date_end)
+    event_term_tweets = {}
+    for date in dates[date_begin_index:date_end_index+1]:
+        for event_term in event_terms:
+            event_term_tweets[event_term].extend(term_date_tweets[event_term][date])
+    # write to file
+    for event_term in event_terms:
+        outfile = outdir + event_term + '_' + date_begin + '-' + date_end + '.txt'
+        with open(outfile, 'w', encoding = 'utf-8') as out:
+            out.write('\n'.join(event_term_tweets[event_term]))
+    if len(event_terms) > 1: # write combis to file
+        combis = query_defs.return_combis(event_terms)
+        combi_tweets = query_defs.extract_andpatterns(event_term_tweets, combis)
+        for combi in combi_tweets.keys():
+            outfile = outdir + combi + '_' + date_begin + '-' + date_end + '.txt'
+            with open(outfile, 'w', encoding = 'utf-8') as out:
+                out.write('\n'.join(combi_tweets[combi]))
---
+++ framework/query_defs.py
@@ -0,0 +1,30 @@
+
+import itertools
+from collections import defaultdict
+
+def return_combis(event_terms):
+    combis = [event_terms]
+    if len(event_terms) > 2:
+        for i in range(2, len(event_terms)):
+            for combi in (list(itertools.combinations(event_terms, i))):
+                 combis.append(list(combi))
+    return combis
+
+def extract_andpatterns(tweetdict, combis):
+    id_tweet = {}
+    event_term_ids = defaultdict(list)
+    for event_term in tweetdict.keys():
+        ids = [x.split('\t')[0] for x in tweetdict[event_term]]
+        for tweet in tweetdict[event_term]:
+            tid = tweet.split('\t')[0]
+            id_tweet[tid] = tweet
+            event_term_ids.append(tid)
+    combi_tweets = {}
+    for combi in combis: # combine
+        combi_sets = []
+        for event_term in combi:
+            combi_sets.append(set(event_term_ids[event_term]))
+        overlap = list(set.intersection(*combi_sets))
+        tweets = [id_tweet[x] for x in overlap]
+        combi_tweets['_'.join(combi)] = tweets
+    return combi_tweets
