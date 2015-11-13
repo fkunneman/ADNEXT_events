2a195013f60553a3067d8763688477a1d5b432c1
fkunneman <f.kunneman@let.ru.nl>
2015-11-13 11:55:07 +0100

    minor fix

--- framework/collect_tweets_keyterms.py
+++ framework/collect_tweets_keyterms.py
@@ -57,21 +57,22 @@ for event in events:
     event_terms = event[0]
     date_begin = event[1]
     date_end = event[2]
-    date_begin_index = dates.index(date_begin)
-    date_end_index = dates.index(date_end)
-    event_term_tweets = {}
-    for date in dates[date_begin_index:date_end_index+1]:
+    if date_begin in dates and date_end in dates:
+        date_begin_index = dates.index(date_begin)
+        date_end_index = dates.index(date_end)
+        event_term_tweets = {}
+        for date in dates[date_begin_index:date_end_index+1]:
+            for event_term in event_terms:
+                event_term_tweets[event_term].extend(term_date_tweets[event_term][date])
+        # write to file
         for event_term in event_terms:
-            event_term_tweets[event_term].extend(term_date_tweets[event_term][date])
-    # write to file
-    for event_term in event_terms:
-        outfile = outdir + event_term + '_' + date_begin + '-' + date_end + '.txt'
-        with open(outfile, 'w', encoding = 'utf-8') as out:
-            out.write('\n'.join(event_term_tweets[event_term]))
-    if len(event_terms) > 1: # write combis to file
-        combis = query_defs.return_combis(event_terms)
-        combi_tweets = query_defs.extract_andpatterns(event_term_tweets, combis)
-        for combi in combi_tweets.keys():
-            outfile = outdir + combi + '_' + date_begin + '-' + date_end + '.txt'
+            outfile = outdir + event_term + '_' + date_begin + '-' + date_end + '.txt'
             with open(outfile, 'w', encoding = 'utf-8') as out:
-                out.write('\n'.join(combi_tweets[combi]))
+                out.write('\n'.join(event_term_tweets[event_term]))
+        if len(event_terms) > 1: # write combis to file
+            combis = query_defs.return_combis(event_terms)
+            combi_tweets = query_defs.extract_andpatterns(event_term_tweets, combis)
+            for combi in combi_tweets.keys():
+                outfile = outdir + combi + '_' + date_begin + '-' + date_end + '.txt'
+                with open(outfile, 'w', encoding = 'utf-8') as out:
+                    out.write('\n'.join(combi_tweets[combi]))
