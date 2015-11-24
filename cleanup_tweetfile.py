62e81500370cc42c6ade42f48e87be5e82eeab4d
Florian Kunneman <f.kunneman@gmail.com>
2015-11-24 13:24:39 +0100

    fl

--- cleanup_tweetfile.py
+++ cleanup_tweetfile.py
@@ -7,7 +7,8 @@ infile = sys.argv[1]
 default_fields = int(sys.argv[2])
 
 with open(infile, 'r', encoding = 'utf-8') as f_in:
-    tweets = f_in.readlines():
+    tweets = f_in.readlines()
 
 cleaned_tweets = gen_functions.cleanup_tweets(tweets, default_fields)
-print(cleaned_tweets)
+with open(infile[:-4] + '_cleaned.txt', 'w', encoding = 'utf-8') as f_out:
+    f_out.write(''.join(cleaned_tweets))
