c0e714659a88e587b0d767c8bb5ea2f4e1bd9157
Florian Kunneman <f.kunneman@gmail.com>
2015-11-25 17:02:46 +0100

    check

--- emotion/score_idf_events.py
+++ emotion/score_idf_events.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 import datahandler
+import featurizer
 
 eventdoc = sys.argv[1]
 outfile = sys.argv[2]
@@ -14,9 +15,8 @@ dh.to_lower()
 
 f = {'token_ngrams' : {'n_list' : [1, 2, 3]}}
 dir_eventdoc = '/'.join(eventdoc.split('/')[:-1])
-featurizer = featurizer.Featurizer(dh['text'], dh['tags'], dir_eventdoc, f)
+featurizer = featurizer.Featurizer(dh.dataset['text'], dh.dataset['tagged'], dir_eventdoc, f)
 featurizer.fit_transform()
 instances, vocabulary = featurizer.return_instances(['token_ngrams'])
 
-print(' | '.join(instances[1]).encode('utf-8')
-
+print(' | '.join(instances[1]).encode('utf-8'))
