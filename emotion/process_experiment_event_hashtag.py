9dae8aa892252ce95e1ba80f1ef9ad0f5c2c5222
Florian Kunneman <f.kunneman@gmail.com>
2015-12-07 16:26:18 +0100

    checked and fixed

--- emotion/process_experiment_event_hashtag.py
+++ emotion/process_experiment_event_hashtag.py
@@ -25,7 +25,7 @@ files_dir = sys.argv[12]
 experiment_dir = sys.argv[13]
 
 def write_config():
-    fileschunks = filesdir.split("/")
+    fileschunks = files_dir.split("/")
     files = "/".join(fileschunks[:-1]) + "/./" + fileschunks[-1]
     current = os.getcwd()
     current_chunks = current.split("/")
@@ -93,40 +93,43 @@ def write_config():
         config_out.write(config)
 
 tmpdir = 'tmp/'
-if not os.exists(tmpdir):
+if not os.path.exists(tmpdir):
     os.mkdir(tmpdir)
-if not os.exists(experiment_dir):
+if not os.path.exists(experiment_dir):
     os.mkdir(experiment_dir)
 event_train_dir = experiment_dir + 'event_train/'
-if not os.exists(event_train_dir):
+if not os.path.exists(event_train_dir):
     os.mkdir(event_train_dir)
 emotion_train_dir = experiment_dir + 'emotion_train/'
-if not os.exists(emotion_train_dir):
+if not os.path.exists(emotion_train_dir):
     os.mkdir(emotion_train_dir)
 
+print('Preparing test')
 # identify dev indices with hashtag
 dr = docreader.Docreader()
 devlines = dr.parse_csv(tweets_dev)
-textlines = [x[-1] for x in devlines]
+textlines_dev = [x[-1] for x in devlines]
 
 cc = coco.Coco(tmpdir)
-cc.set_lines(textlines)
+cc.set_lines(textlines_dev)
 cc.set_file()
 cc.model_ngramperline([hashtag])
 matches = cc.match([hashtag])[hashtag]
 
-ht_tweets_dev = [lines[i] for i in matches]
+#matches = random.sample(range(len(textlines_dev)), 1000)
+
+ht_tweets_dev = [devlines[i] for i in matches]
 with open(parts_dev) as dev_open:
-    parts = dev_open.read().split('\n')
+    parts = dev_open.readlines()
 
 test = []
-for i, instance in parts:
+for i, instance in enumerate(parts):
     tokens = instance.split()
     if i in matches:
         test.append(tokens[0] + ' ' + label_positive)
     else:
         test.append(tokens[0] + ' ' + label_negative)
-test_tuples = [instance.split() for instance in testparts]
+test_tuples = [instance.split() for instance in test]
 targets = dict((filename, target) for filename, target in test_tuples)
 
 # identify train indices with hashtag
@@ -139,6 +142,8 @@ cc.set_lines(textlines)
 cc.set_file()
 cc.model_ngramperline([hashtag])
 matches = cc.match([hashtag])[hashtag]
+
+#matches = random.sample(range(len(textlines)), 2000)
 
 # make new files with matches
 ht_tweets_train = [textlines[i] for i in matches]
@@ -159,8 +164,8 @@ for tweet in ht_tweets_train_tagged:
     new_tweets_tagged.append(new_tweet)
 
 print('Extracting features')
-partsdirectory = '/'.join(parts_hashtag.split('/')[:-1])
-featuredict = {'token_ngrams' : {'n_list': [1, 2, 3], 'blackfeats' : ['_USER_', '_URL_'] + hashtag}}
+partsdirectory = '/'.join(parts_hashtag.split('/')[:-1]) + '/'
+featuredict = {'token_ngrams' : {'n_list': [1, 2, 3], 'blackfeats' : ['_USER_', '_URL_'] + [hashtag]}}
 ft = featurizer.Featurizer(ht_tweets_train, new_tweets_tagged, partsdirectory, featuredict)
 ft.fit_transform()
 instances, vocabulary = ft.return_instances(['token_ngrams'])
@@ -177,21 +182,20 @@ for i, chunk in enumerate(chunks):
     for j, index in enumerate(chunk):
         zeros = 5 - len(str(j))
         filename = subpart + ('0' * zeros) + str(j) + '.txt'
-        print(index, instances.shape[0])
         features = [vocabulary[x] for x in instances[index].indices]
         with open(files_dir + filename, 'w', encoding = 'utf-8') as outfile: 
             outfile.write('\n'.join(features))
         train.append(filename + ' ' + label_positive)
 
 with open(parts_train) as train_open:
-    parts = train_open.read().split('\n')
+    parts = train_open.readlines()
 
 parts_indices = range(len(parts))
 nht_train = list(set(parts_indices) - set(matches))
 num_ht_train = len(matches)
 random_train = random.sample(nht_train, num_ht_train)
 
-for i, instance in parts:
+for i, instance in enumerate(parts):
     if i in random_train:
         tokens = instance.split()
         train.append(tokens[0] + ' ' + label_negative)
@@ -213,10 +217,10 @@ with open('test.rnk') as rnk:
         filename = tokens[0].strip()
         classification, score = tokens[1].split()[0].split(":")
         classification = classification.replace("?","")
-        classifications.append([targets[filename], classification, score])
+        classifications.append([targets[filename], classification, float(score)])
 
 os.system("mv * " + event_train_dir)
-output = (devlines, [classifications, '-', '-']) 
+output = (textlines_dev, [classifications, False, False]) 
 rp = reporter.Eval(output, [label_positive, label_negative], event_train_dir)
 rp.report()
 
@@ -228,38 +232,32 @@ train_tweets_general = dr.parse_csv(tweets_hashtag)
 train_tweets_ids = [x[0] for x in train_tweets_general]
 ids_dev = set([x[0] for x in devlines])
 overlap = [] 
-for i, tweet in train_tweets_ids:
-    if tweet & ids_dev:
+for i, tweet in enumerate(train_tweets_ids):
+    if set([tweet]) & ids_dev:
         overlap.append(i)
-print('overlapping tweets hashtag:', overlap)
 
 with open(parts_hashtag) as ph_open:
-    train_parts_general = ph_open.read().split('\n')
+    train_parts_general = ph_open.readlines()
 print('train size before:', len(train_parts_general))
 train_parts_general_clean = [x for i, x in enumerate(train_parts_general) if not i in overlap]
 print('train size after:', len(train_parts_general_clean))
 
 dr = docreader.Docreader()
 random_tweets = dr.parse_csv(tweets_random)
-random_tweets_ids = [x[0] for x in random_tweets_general]
+random_tweets_ids = [x[0] for x in random_tweets]
 overlap = [] 
-for i, tweet in random_tweets_ids:
-    if tweet & ids_dev:
-        overlap.append(i)
-print('overlapping tweets random:', overlap)
-
+for i, tweet in enumerate(random_tweets_ids):
+    if set([tweet]) & ids_dev:
+        overlap.append(i)   
 with open(parts_random) as pr_open:
-    random_parts = pr_open.read().split('\n')
+    random_parts = pr_open.readlines()
 print('random size before:', len(random_parts))
 random_parts_clean = [x for i, x in enumerate(random_parts) if not i in overlap]
 print('random size after:', len(random_parts_clean))
 
 random_sample = random.sample(random_parts_clean, len(train_parts_general_clean))
-random_sample_correct_label = []
-for x in random_sample:
-    tokens = x.split()
-with open(train, 'w', encoding = 'utf-8') as train_out:
-    train_out.write('\n'.join(train_parts_general_clean))
+with open('train', 'w', encoding = 'utf-8') as train_out:
+    train_out.write(''.join(train_parts_general_clean))
     for x in random_sample:
         tokens = x.strip().split()
         train_out.write('\n' + tokens[0] + ' ' + label_negative)
@@ -279,9 +277,9 @@ with open('test.rnk') as rnk:
         filename = tokens[0].strip()
         classification, score = tokens[1].split()[0].split(":")
         classification = classification.replace("?","")
-        classifications.append([targets[filename], classification, score])
+        classifications.append([targets[filename], classification, float(score)])
 
 os.system("mv * " + emotion_train_dir)
-output = (devlines, [classifications, '-', '-']) 
+output = (textlines_dev, [classifications, False, False]) 
 rp = reporter.Eval(output, [label_positive, label_negative], emotion_train_dir)
 rp.report()
