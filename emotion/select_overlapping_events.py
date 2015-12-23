5296b2a97fa00acf36f29d51ac43e5ea80517a78
fkunneman <f.kunneman@let.ru.nl>
2015-12-23 22:00:12 +0100

    update

--- emotion/select_overlapping_events.py
+++ emotion/select_overlapping_events.py
@@ -5,7 +5,8 @@ import sys
 import docreader
 
 eventdir = sys.argv[1]
-outfile = sys.argv[2]
+outfile_overlaps = sys.argv[2]
+outfile_no_overlap = sys.argv[3]
 
 eventfiles = os.listdir(eventdir)
 
@@ -18,18 +19,28 @@ for eventfile in eventfiles:
 
 num_events = len(event_ids)
 overlaps = []
+event_overlap = {}
+for event in eventfiles:
+    event_overlap[event] = False
 for i, event in enumerate(event_ids[-1]):
     j = i + 1
     while j < num_events:
         event2 = event_ids[j]
         print(i, '+', j, 'of', num_events)
-        if set(event[1]) & set(event2[1]): 
+        if set(event[1]) & set(event2[1]):
+            event_overlap[event[0]] = True
+            event_overlap[event2[0]] = True
             num_overlap1 = len(list(set(event[1]) - set(event2[1])))
             percent_overlap1 = num_overlap1 / len(event[1])
             num_overlap2 = len(list(set(event2[1]) - set(event[1])))
             percent_overlap2 = num_overlap2 / len(event2[1])
-            overlaps.append([num_overlap1, percent_overlap1, num_overlap2, percent_overlap2])
+            overlaps.append([event[0], event2[0], num_overlap1, percent_overlap1, num_overlap2, percent_overlap2])
 
-with open(outfile, 'w') as w_out:
+with open(outfile_overlaps, 'w') as w_out:
     for ov in overlaps:
         w_out.write(' '.join(ov) + '\n')
+
+with open(outfile_no_overlap, 'w') as w_out:
+    for event in eventfiles:   
+        if not event_overlap[event]:
+            w_out.write(event + '\n')
