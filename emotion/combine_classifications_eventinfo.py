cc09b96d4318a8ce86ad195f2c6e97be0d73edb3
fkunneman <f.kunneman@let.ru.nl>
2016-02-04 16:16:10 +0100

    fixes

--- emotion/combine_classifications_eventinfo.py
+++ emotion/combine_classifications_eventinfo.py
@@ -21,10 +21,11 @@ eventfiles = os.listdir(event_info_dir)
 before_tweets = os.listdir(event_dir_before)
 after_tweets = os.listdir(event_dir_after)
 
-print('collecting event data')
+print('collecting event data before')
 for event in classification_files_before[:1]:
     event_id = event[:-4]
     print(event_id)
+    new_tweets = []
     with open(classification_dir_before + event) as classifications_in:
         lines = classifications_in.readlines()
         file_score = emotion_utils.parse_lcs_classifications(lines, target_before)
@@ -34,6 +35,42 @@ for event in classification_files_before[:1]:
     dr.parse_doc(doc_tweets)
     for f in files:
         index = emotion_utils.filename2tweetindex(f)
-        print(f, index)
+        tweet = dr.lines[index]
+        new_tweets.append([float(file_score[f]), tweet[0], tweet[2], tweet[3], tweet[4]])
+    new_tweets_sorted = sorted(new_tweets, key = lambda k : k[0], reverse = True)
+    eventfile = event_info_dir + 'sequence_' + event_id + '.txt'
+    with open(eventfile, 'r', encoding = 'utf-8') as event_in:
+        tokens = event_in.readlines().split('\t')
+        event_terms = tokens[0].split(', ') 
+        event_date = tokens[1]
+        with open(outdir + event_id + '_' + target_before + '.txt', 'w', encoding = 'utf-8') as sout:
+            sout.write(event_terms + '\t' + event_date + '\n')
+            sout.write('\n'.join(['\t'.join(x) for x in new_tweets_sorted))
+
+print('collecting event data after')
+for event in classification_files_after[:1]:
+    event_id = event[:-4]
+    print(event_id)
+    new_tweets = []
+    with open(classification_dir_after + event) as classifications_in:
+        lines = classifications_in.readlines()
+        file_score = emotion_utils.parse_lcs_classifications(lines, target_after)
+    files = sorted(file_score.keys())
+    doc_tweets = event_dir_after + 'tweets_' + event_id + '.csv'
+    dr = docreader.Docreader()
+    dr.parse_doc(doc_tweets)
+    for f in files:
+        index = emotion_utils.filename2tweetindex(f)
+        tweet = dr.lines[index]
+        new_tweets.append([float(file_score[f]), tweet[0], tweet[2], tweet[3], tweet[4]])
+    new_tweets_sorted = sorted(new_tweets, key = lambda k : k[0], reverse = True)
+    eventfile = event_info_dir + 'sequence_' + event_id + '.txt'
+    with open(eventfile, 'r', encoding = 'utf-8') as event_in:
+        tokens = event_in.readlines().split('\t')
+        event_terms = tokens[0].split(', ') 
+        event_date = tokens[1]
+        with open(outdir + event_id + '_' + target_after + '.txt', 'w', encoding = 'utf-8') as sout:
+            sout.write(event_terms + '\t' + event_date + '\n')
+            sout.write('\n'.join(['\t'.join(x) for x in new_tweets_sorted))
 
 
