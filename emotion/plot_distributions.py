9911438378c8893ecb015a540413ef250df1d3b3
fkunneman <f.kunneman@let.ru.nl>
2016-02-08 13:58:03 +0100

    ne

---
+++ emotion/plot_distributions.py
@@ -0,0 +1,53 @@
+
+import os
+import sys
+import matplotlib.pyplot as plt
+
+import docreader
+
+classificationdir = sys.argv[1]
+plot_out = sys.argv[2]
+event_threshold = sys.argv[3]
+
+classifications = os.listdir(classificationdir)
+events = list(set([x.split('_')[0] for x in classifications]))
+
+new_scores = []
+for event in events:
+    print(event)
+    # zin
+    try:
+        dr_zin = docreader.Docreader()
+        dr_zin.parse_doc(classificationdir + event + '_zin.txt')
+    except:
+        continue
+    if len(dr_zin.lines) > event_threshold:
+        zin_tweets = dr_zin.lines[1:]
+        zin_scores = [float(tweet[0]) for tweet in zin_tweets]
+        plt.hist(zin_scores)
+        plt.savefig(plot_out + 'dist_zin_' + event + '.png')
+        plt.clf()
+    # teleurgesteld
+    try:
+        dr_teleurgesteld = docreader.Docreader()
+        dr_teleurgesteld.parse_doc(classificationdir + event + '_teleurgesteld.txt')
+    except:
+        continue
+    if len(dr_teleurgesteld.lines) > event_threshold:
+        teleurgesteld_tweets = dr_teleurgesteld.lines[1:]
+        teleurgesteld_scores = [float(tweet[0]) for tweet in teleurgesteld_tweets]
+        plt.hist(teleurgesteld_scores)
+        plt.savefig(plot_out + 'dist_teleurgesteld_' + event + '.png')
+        plt.clf()
+    # tevreden
+    try:
+        dr_tevreden = docreader.Docreader()
+        dr_tevreden.parse_doc(classificationdir + event + '_tevreden.txt')
+    except:
+        continue
+    if len(dr_tevreden.lines) > event_threshold:
+        tevreden_tweets = dr_tevreden.lines[1:]
+        tevreden_scores = [float(tweet[0]) for tweet in tevreden_tweets]
+        plt.hist(tevreden_scores)
+        plt.savefig(plot_out + 'dist_tevreden_' + event + '.png')
+        plt.clf()
