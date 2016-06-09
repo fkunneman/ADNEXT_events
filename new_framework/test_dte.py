fa94b8caa79265c515797521850e50f10e9dbe0f
Florian Kunneman <f.kunneman@gmail.com>
2016-06-09 14:27:36 +0200

    fixes

--- new_framework/test_dte.py
+++ new_framework/test_dte.py
@@ -11,7 +11,11 @@ with open(test_tweets, 'r', encoding = 'utf-8') as test_in:
 
 for tweet in tweets:
     tokens = tweet.strip().split('\t')
-    date = time_functions.return_datetime(tokens[0])
+    try:
+        date = time_functions.return_datetime(tokens[0], setting = 'vs')
+    except:
+        print('except:', tokens[0])
+        continue
     text = tokens[1]
     dte = dutch_timex_extractor.Dutch_timex_extractor(text, date)
     dte.extract_date()
